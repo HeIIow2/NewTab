1971a41c9777da72b4b2b0f13a84c5b5ca393f30
Hellow <Lars.Noack@outlook.de>
2023-02-25 14:36:01 +0100

    saved the last used search engine in the cookies, and fixed design in chromium

--- src/js/individual.js
+++ src/js/individual.js
@@ -1,5 +1,4 @@
-let cards;
-let card_pos = [];
+
 
 
 function random(min, max) {
@@ -26,57 +25,18 @@ function hide_catgirl() {
 function onload() {
     catgirl_element = document.getElementById("popup-catgirl");
     setTimeout(show_catgirl, random(10000, 100000));
-
-    cards = document.getElementsByClassName("card-wrapper")
-    for (const c of cards) {
-        card_pos.push(getCoords(c));
-    }
-    console.log(card_pos)
 }
 
 
 let dde = document.documentElement;
 
 dde.addEventListener("mousemove", e => {
-    if (cards != null) {
-        modify_card_size(e);
-    }
+    parallax(e);
 });
 
-function getCoords(elem) { // crossbrowser version
-    var box = elem.getBoundingClientRect();
-
-    var body = document.body;
-    var docEl = document.documentElement;
-
-    var scrollTop = window.pageYOffset || docEl.scrollTop || body.scrollTop;
-    var scrollLeft = window.pageXOffset || docEl.scrollLeft || body.scrollLeft;
-
-    var clientTop = docEl.clientTop || body.clientTop || 0;
-    var clientLeft = docEl.clientLeft || body.clientLeft || 0;
-
-    var top  = box.top +  scrollTop - clientTop;
-    var left = box.left + scrollLeft - clientLeft;
-
-    return { top: Math.round(top), left: Math.round(left) };
-}
-
-
-
-function modify_card_size(e) {
-    const x = e.clientX;
-    const y = e.clientY;
-    for (var i=0; i<cards.length; i++) {
-        const card = cards[i];
-        const x_c = card_pos[i]["left"];
-        const y_c = card_pos[i]["top"];
-        const distance = Math.abs(x - x_c) + Math.abs(y - y_c);
-
-        let size = 100 - (distance/20);
-        if (size < 50) {
-            size = 50;
-        }
-        card.style.setProperty("--width", size.toString() + "%");
-        card.style.setProperty("--height", size.toString() + "%");
-    }
+function parallax(e) {
+    let ow = dde.offsetWidth;
+    let oh = dde.offsetHeight;
+    dde.style.setProperty('--mouseX', e.clientX * 10 / ow + "%");
+    dde.style.setProperty('--mouseY', e.clientY * 10 / oh + "%");
 }
--- src/js/search.js
+++ src/js/search.js
@@ -4,10 +4,11 @@ const all_options_elem = document.getElementById("engine-elem");
 const option_elements = all_options_elem.getElementsByClassName("option");
 
 
+const LAST_USED_COOKIE_NAME = "engine"
+
 
 // add event listeners
 form_elem.addEventListener("keydown", function (e) {
-    console.log(e.key)
     if (e.key === 'Enter') {
         submit();
     } else if (e.key === 'ArrowUp') {
@@ -27,15 +28,25 @@ function clicked_option(option) {
 }
 
 function set_engine(object, first_to_last = false) {
-    console.log(object)
     const clicked_id = object.getAttribute("id");
+    console.log("Setting search engine to: " + clicked_id);
+
     all_options_elem.setAttribute("value", clicked_id)
+
+    // set the cookie to the engine.
+    document.cookie = LAST_USED_COOKIE_NAME + "=" + clicked_id;
+
     // move the clicked element to the top of all children
     all_options_elem.insertBefore(object, all_options_elem.firstChild);
 
     if (first_to_last) {
         all_options_elem.appendChild(all_options_elem.children[1]);
     }
+}
+
+function set_engine_to_name(engine_name, first_to_last = false) {
+    let engine_object = document.getElementById(engine_name);
+    set_engine(engine_object, first_to_last=first_to_last);
 }
 
 // if form got submitted
@@ -63,6 +74,7 @@ function submit() {
     }
 
     const base_urls = {
+        "nix": "https://search.nixos.org/packages?channel=22.11&from=0&size=50&sort=relevance&type=packages&query=",
         "duck-duck-go": "https://duckduckgo.com/?q=",
         "google": "https://www.google.com/search?q=",
         "youtube": "https://www.youtube.com/results?search_query=",
@@ -81,4 +93,33 @@ function go_to(url) {
     setTimeout(function () {
         window.open(url, "_self");
     });
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+}
+
+
+// loading last used engine, if the cookie can be found
+let engine_name_from_cookie = getCookie(LAST_USED_COOKIE_NAME);
+if (engine_name_from_cookie !== "") {
+    set_engine_to_name(engine_name_from_cookie);
+} else {
+    // if the cookie has not been set yet it needs to load the default search engine, 
+    // which just is the first option in the html list
+
+    let firstOption = document.getElementById("engine-elem").querySelector(".option");
+    set_engine(firstOption);
 }
